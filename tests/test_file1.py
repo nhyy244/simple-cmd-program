107c5989ff54ad6cc81f69ebdc62b7152acb43ab
nhyy244 <narcys0814@gmail.com>
2026-01-24 17:16:23 +0100

    added another test =))

--- tests/test_file1.py
+++ tests/test_file1.py
@@ -1,9 +1,9 @@
 import pytest
 import random
+import art
 from src import app,flag_behaviours
 from src.flag_descriptions import help_description, ascii_description
 class TestClass:  
-    random.seed(5) #for ascii tests
     def test_zero_arguments(self, capsys):
       with pytest.raises(SystemExit):
         app.parse_args(([], []))
@@ -78,6 +78,16 @@ class TestClass:
       greetings.pop(0)
       for i, deafult_behaviour_res in enumerate(out):
         assert deafult_behaviour_res == f"hello {greetings[i]}"
+        
+    def test_ascii_flag(self,capsys):# TODO mock randomness or do not test for randomness at all. will have problems with seed for multiple ascii greetings
+      greetings = ["file_path","greeting1"]
+      with pytest.raises(SystemExit):
+        random.seed(5) #for ascii tests
+        app.parse_args((greetings,["-a"]))
+      out = str(capsys.readouterr().out)
+      random.seed(5) #for ascii tests
+      expected_res = art.text2art(f"hello {greetings[1]}", font='random')  
+      assert expected_res.strip() == out.strip()
     
       
 
